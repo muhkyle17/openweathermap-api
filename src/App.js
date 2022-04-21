24594ac3f2401dd003c699df65e0e3a2920be718
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-04-21 16:04:11 -0700

    Initialize project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+import './App.css'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='flex items-center justify-center w-screen h-screen'>
+      <div className='flex flex-col h-64 w-60 items-center justify-around bg-red-200'>
+        <p>Tuesday</p>
+        <p>Icon</p>
+        <div className='flex justify-between w-1/2'>
+          <p>27</p>
+          <p>26</p>
+        </div>
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
