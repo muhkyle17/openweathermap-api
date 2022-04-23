295c08851bcea5a4ee65b0ac29b3909f0d421f84
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-04-23 14:20:07 -0700

    Make code work

--- src/App.js
+++ src/App.js
@@ -6,28 +6,10 @@ import { useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const today = new Date()
-  const dayOfWeekNumber = today.getDay()
-  let nameOfDay
-
-  if (dayOfWeekNumber === 1) {
-    nameOfDay = 'Monday'
-  } else if (dayOfWeekNumber === 2) {
-    nameOfDay = 'Tuesday'
-  } else if (dayOfWeekNumber === 3) {
-    nameOfDay = 'Wednesday'
-  } else if (dayOfWeekNumber === 4) {
-    nameOfDay = 'Thursday'
-  } else if (dayOfWeekNumber === 5) {
-    nameOfDay = 'Friday'
-  } else {
-    console.log('Nothing')
-  }
-
   const API_TOKEN = process.env.REACT_APP_API_KEY
   const API =
-    'https://api.openweathermap.org/data/2.5/onecall?lat=49.2608724&lon=-123.113952&exclude=minutely,hourly,alerts&appid='
-  const [weatherIcon, setWeatherIcon] = useState('')
+    'https://api.openweathermap.org/data/2.5/onecall?lat=49.2608724&lon=-123.113952&units=metric&exclude=minutely,hourly,alerts&appid='
+  const [weatherDays, setWeatherDays] = useState([])
 
   useEffect(() => {
     axios({
@@ -36,27 +18,84 @@ function App() {
     })
       .then((res) => {
         data = res.data
-        console.log(data)
-        setWeatherIcon(data.current.weather[0].icon)
+
+        const days = data.daily
+        console.log(days)
+        setWeatherDays(days)
       })
       .catch((err) => {
         console.log(err)
       })
   }, [])
 
+  const days = [
+    'Sunday',
+    'Monday',
+    'Tuesday',
+    'Wednesday',
+    'Thursday',
+    'Friday',
+    'Saturday',
+  ]
+
+  function consecutiveDays(days, index) {
+    const today = new Date()
+    const tomorrow = new Date(today)
+    if (index === 0) {
+      tomorrow.setDate(tomorrow.getDate())
+      const dayName = days[tomorrow.getDay()]
+      return <p>{dayName}</p>
+    } else if (index === 1) {
+      tomorrow.setDate(tomorrow.getDate() + 1)
+      const dayName = days[tomorrow.getDay()]
+      return <p>{dayName}</p>
+    } else if (index === 2) {
+      tomorrow.setDate(tomorrow.getDate() + 2)
+      const dayName = days[tomorrow.getDay()]
+      return <p>{dayName}</p>
+    } else if (index === 3) {
+      tomorrow.setDate(tomorrow.getDate() + 3)
+      const dayName = days[tomorrow.getDay()]
+      return <p>{dayName}</p>
+    } else if (index === 4) {
+      tomorrow.setDate(tomorrow.getDate() + 4)
+      const dayName = days[tomorrow.getDay()]
+      return <p>{dayName}</p>
+    } else if (index === 5) {
+      tomorrow.setDate(tomorrow.getDate() + 5)
+      const dayName = days[tomorrow.getDay()]
+      return <p>{dayName}</p>
+    } else if (index === 6) {
+      tomorrow.setDate(tomorrow.getDate() + 6)
+      const dayName = days[tomorrow.getDay()]
+      return <p>{dayName}</p>
+    } else if (index === 7) {
+      tomorrow.setDate(tomorrow.getDate() + 7)
+      const dayName = days[tomorrow.getDay()]
+      return <p>{dayName}</p>
+    } else {
+      console.log('Please work')
+    }
+  }
+
   return (
     <div className='flex items-center justify-center w-screen h-screen'>
-      <div className='flex flex-col h-auto py-6 w-60 items-center justify-around bg-red-200'>
-        <p>{nameOfDay}</p>
-        <img
-          src={`http://openweathermap.org/img/wn/${weatherIcon}@2x.png`}
-          alt='Weather Icon'
-        />
-        <div className='flex justify-between w-1/2'>
-          <p>27</p>
-          <p>26</p>
+      {weatherDays.map((day, index) => (
+        <div
+          className='flex flex-col h-auto py-6 w-60 items-center justify-around bg-red-200'
+          key={index}
+        >
+          {consecutiveDays(days, index)}
+          <img
+            src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+            alt='Weather Icon'
+          />
+          <div className='flex justify-between w-1/2'>
+            <p>{Math.trunc(day.temp.min)}&#176;</p>
+            <p>{Math.trunc(day.temp.max)}&#176;</p>
+          </div>
         </div>
-      </div>
+      ))}
     </div>
   )
 }
