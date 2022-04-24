a3a05b89118882c4d6e6fa07fad86f6a9b005de0
Mikyle Reyes <reyesmikyle17@gmail.com>
2022-04-23 17:43:54 -0700

    Final changes to the weather app

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const API =
     'https://api.openweathermap.org/data/2.5/onecall?lat=49.2608724&lon=-123.113952&units=metric&exclude=minutely,hourly,alerts&appid='
   const [weatherDays, setWeatherDays] = useState([])
+  const [dayToday, setDayToday] = useState('')
 
   useEffect(() => {
     axios({
@@ -44,58 +45,125 @@ function App() {
     if (index === 0) {
       tomorrow.setDate(tomorrow.getDate())
       const dayName = days[tomorrow.getDay()]
-      return <p>{dayName}</p>
+      return <p className='text-2xl'>{dayName.slice(0, 3)}</p>
     } else if (index === 1) {
       tomorrow.setDate(tomorrow.getDate() + 1)
       const dayName = days[tomorrow.getDay()]
-      return <p>{dayName}</p>
+      return <p className='text-2xl'>{dayName.slice(0, 3)}</p>
     } else if (index === 2) {
       tomorrow.setDate(tomorrow.getDate() + 2)
       const dayName = days[tomorrow.getDay()]
-      return <p>{dayName}</p>
+      return <p className='text-2xl'>{dayName.slice(0, 3)}</p>
     } else if (index === 3) {
       tomorrow.setDate(tomorrow.getDate() + 3)
       const dayName = days[tomorrow.getDay()]
-      return <p>{dayName}</p>
+      return <p className='text-2xl'>{dayName.slice(0, 3)}</p>
     } else if (index === 4) {
       tomorrow.setDate(tomorrow.getDate() + 4)
       const dayName = days[tomorrow.getDay()]
-      return <p>{dayName}</p>
+      return <p className='text-2xl'>{dayName.slice(0, 3)}</p>
     } else if (index === 5) {
       tomorrow.setDate(tomorrow.getDate() + 5)
       const dayName = days[tomorrow.getDay()]
-      return <p>{dayName}</p>
+      return <p className='text-2xl'>{dayName.slice(0, 3)}</p>
     } else if (index === 6) {
       tomorrow.setDate(tomorrow.getDate() + 6)
       const dayName = days[tomorrow.getDay()]
-      return <p>{dayName}</p>
+      return <p className='text-2xl'>{dayName.slice(0, 3)}</p>
     } else if (index === 7) {
       tomorrow.setDate(tomorrow.getDate() + 7)
       const dayName = days[tomorrow.getDay()]
-      return <p>{dayName}</p>
+      return <p className='text-2xl'>{dayName.slice(0, 3)}</p>
+    } else {
+      console.log('Please work')
+    }
+  }
+
+  useEffect(() => {
+    const today = new Date()
+    const tomorrow = new Date(today)
+    tomorrow.setDate(tomorrow.getDate())
+    const dayName = days[tomorrow.getDay()]
+    setDayToday(dayName)
+  }, [])
+
+  function colorDayNumber(days, index) {
+    const today = new Date()
+    const tomorrow = new Date(today)
+    if (index === 0) {
+      tomorrow.setDate(tomorrow.getDate())
+      const dayName = days[tomorrow.getDay()]
+      return dayName === dayToday ? 'bg-gray-300' : 'bg-gray-200'
+    } else if (index === 1) {
+      tomorrow.setDate(tomorrow.getDate() + 1)
+      const dayName = days[tomorrow.getDay()]
+      if (dayName === dayToday) {
+        return 'bg-gray-300'
+      }
+    } else if (index === 2) {
+      tomorrow.setDate(tomorrow.getDate() + 2)
+      const dayName = days[tomorrow.getDay()]
+      if (dayName === dayToday) {
+        return 'bg-gray-300'
+      }
+    } else if (index === 3) {
+      tomorrow.setDate(tomorrow.getDate() + 3)
+      const dayName = days[tomorrow.getDay()]
+      if (dayName === dayToday) {
+        return 'bg-gray-300'
+      }
+    } else if (index === 4) {
+      tomorrow.setDate(tomorrow.getDate() + 4)
+      const dayName = days[tomorrow.getDay()]
+      if (dayName === dayToday) {
+        return 'bg-gray-300'
+      }
+    } else if (index === 5) {
+      tomorrow.setDate(tomorrow.getDate() + 5)
+      const dayName = days[tomorrow.getDay()]
+      if (dayName === dayToday) {
+        return 'bg-gray-300'
+      }
+    } else if (index === 6) {
+      tomorrow.setDate(tomorrow.getDate() + 6)
+      const dayName = days[tomorrow.getDay()]
+      if (dayName === dayToday) {
+        return 'bg-gray-300'
+      }
+    } else if (index === 7) {
+      tomorrow.setDate(tomorrow.getDate() + 7)
+      const dayName = days[tomorrow.getDay()]
+      if (dayName === dayToday) {
+        return 'bg-gray-300'
+      }
     } else {
       console.log('Please work')
     }
   }
 
   return (
-    <div className='flex items-center justify-center w-screen h-screen'>
-      {weatherDays.map((day, index) => (
-        <div
-          className='flex flex-col h-auto py-6 w-60 items-center justify-around bg-red-200'
-          key={index}
-        >
-          {consecutiveDays(days, index)}
-          <img
-            src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
-            alt='Weather Icon'
-          />
-          <div className='flex justify-between w-1/2'>
-            <p>{Math.trunc(day.temp.min)}&#176;</p>
-            <p>{Math.trunc(day.temp.max)}&#176;</p>
+    <div className='flex items-center justify-center w-screen h-screen bg-gray-200'>
+      <div className='flex items-center justify-center border border-gray-300'>
+        {weatherDays.slice(0, 5).map((day, index) => (
+          <div
+            className={`flex flex-col h-auto py-6 w-60 items-center justify-around  ${colorDayNumber(
+              days,
+              index
+            )}`}
+            key={index}
+          >
+            {consecutiveDays(days, index)}
+            <img
+              src={`http://openweathermap.org/img/wn/${day.weather[0].icon}@2x.png`}
+              alt='Weather Icon'
+            />
+            <div className='flex gap-7'>
+              <p className='text-3xl'>{Math.trunc(day.temp.min)}&#176;</p>
+              <p className='text-3xl'>{Math.trunc(day.temp.max)}&#176;</p>
+            </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   )
 }
